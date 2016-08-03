7673d8f0263e02fcc9760eff6a96344b93220e3c
davedivilly <davedivilly@gmail.com>
2016-08-03 14:48:22 +0100

    Update lv-hyperic-agent-switch.py

--- py_scripts/lv-hyperic-agent-switch.py
+++ py_scripts/lv-hyperic-agent-switch.py
@@ -6,9 +6,9 @@ from fabric.api import *
 
 # Get lisy of hosts into a txt file
 #os.system("mdb admin_contact=corp.web.admin office_location=LAS.COLO2 return alias | grep alias | awk -F' ' '{print$2}' | sort | grep -v prd | grep -v x  > ~/SCRIPTS/PYTHON/non-prd-internal.txt")
-print "-----------------------------------------------------------------------------"
-print "To use this script type: fab -f lv-hyperic-agent-switch.py hosts agent_setup "
-print "-----------------------------------------------------------------------------"
+print "----------------------------------------------------------------"
+print "To use this script type: fab -f lv-hyperic-agent-switch.py task "
+print "----------------------------------------------------------------"
 
 # used to prevent annoying errors when default is used "/bin/bash -l -c"
 env.shell = "/bin/bash -c"
@@ -32,3 +32,6 @@ def agent_setup():
              print "Hyperic-lv Non-Prod Agent has been setup on:", env.host_string 
              print "--------------------------------------------"
 	
+def task():
+	execute(hosts)
+	execuet(agent_setup)
