c57cb703bcfee5804225fc3e4d3f7f0ec8b3ef54
davedivilly <davedivilly@gmail.com>
2016-08-03 14:53:39 +0100

    Update restart-agent-dmz.py

--- py_scripts/restart-agent-dmz.py
+++ py_scripts/restart-agent-dmz.py
@@ -1,5 +1,4 @@
 #!/usr2/ddivilly/PYTHON/bin/python
-
 import os
 import sys
 from fabric.api import * 
@@ -7,9 +6,9 @@ from fabric.api import *
 # Get lisy of hosts into a txt file
 #os.system("mdb admin_contact=corp.web.admin office_location=LAS.COLO2 return alias | grep alias | awk -F' ' '{print$2}' | sort | grep -v prd | grep -v x |grep switch  > ~/SCRIPTS/PYTHON/remove_restart.txt")
 
-print "--------------------------------------------------------------"
-print "To use this script type: fab -f restart-agent.py hosts restart"
-print "--------------------------------------------------------------"
+print "-----------------------------------------------------"
+print "To use this script type: fab -f restart-agent.py task"
+print "-----------------------------------------------------"
 
 # used to prevent annoying errors when default is used "/bin/bash -l -c"
 env.shell = "/bin/bash -c"
@@ -27,3 +26,7 @@ def restart():
 	print "-------------------------------------------"
         print "SD agent restarted on : ", env.host_string
         print "-------------------------------------------"
+
+def task():
+	execute(hosts)
+	execute(restart)
