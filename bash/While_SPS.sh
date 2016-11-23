f1cf76d5ba9cabc149651dadeea11ebe6074a80c
dave <davedivilly@users.noreply.github.com>
2016-11-23 17:41:13 +0000

    Update While_SPS.sh

--- bash/While_SPS.sh
+++ bash/While_SPS.sh
@@ -3,11 +3,13 @@
 
 while [ `sudo netstat -tulpn | grep -c '8100\|8200\|8300'` -ge 1 2>/dev/null ]; do
         logger "Waiting for SPS to STOP"
+        echo "Waiting for SPS to STOP"
           sleep 3
         if [ `sudo netstat -tulpn | grep -c '8100\|8200\|8300'` -ge 1 2>/dev/null ]; then
           continue
         fi
         logger "SPS has been successfully stopped."
+        echo "SPS has been successfully stopped."
 done
 
 exit 0
