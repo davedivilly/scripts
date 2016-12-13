6b745ac9d2ad3da8c5f46b93ab864eae990cbe87
dave <davedivilly@users.noreply.github.com>
2016-12-13 10:17:19 +0000

    Update While_SPS.sh

--- bash/While_SPS.sh
+++ bash/While_SPS.sh
@@ -1,15 +1,22 @@
 # Script that makes sure SPS Tomcat has stopped
 #!/bin/bash
 
-while [ `sudo netstat -tulpn | grep -c '8100\|8200\|8300'` -ge 1 2>>error.log ]; do
+x=1
+while [ `netstat -tulpn | grep -c '8100\|8200\|8300'` -ge 1 ] && [ $x -le 100 ]; do
         logger "Waiting for SPS to STOP"
         echo "Waiting for SPS to STOP"
           sleep 3
-        if [ `sudo netstat -tulpn | grep -c '8100\|8200\|8300'` -ge 1 2>>error.log ]; then
-          continue
+        if [ `netstat -tulpn | grep -c '8100\|8200\|8300'` -ge 1 ]; then
+          x=$(( $x + 1 ))
+          if [ $x -eq 100 ]; then
+           logger "Loop detected. Processes have not shutdown after 5 mins. Aborting Script"
+           echo "Loop detected. Processes have not shutdown after 5 mins. Aborting Script"
+           exit 1
+             else
+              continue
+          fi
         fi
         logger "SPS has been successfully stopped."
         echo "SPS has been successfully stopped."
-done
-
-exit 0
+       done
+ exit 0
